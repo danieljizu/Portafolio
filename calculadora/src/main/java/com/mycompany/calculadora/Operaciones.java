fc07d64044147c0d384d187e68cd937a9b2a5096
Daniel jimenez <Daniel_jimenez25@hotmail.com>
2022-12-01 13:56:14 -0500

    funcion resta

--- calculadora/src/main/java/com/mycompany/calculadora/Operaciones.java
+++ calculadora/src/main/java/com/mycompany/calculadora/Operaciones.java
@@ -2,15 +2,16 @@ package com.mycompany.calculadora;
 
 public class Operaciones {
 
-    int arg1 = 0;
-    int arg2 = 0;
-
     public Operaciones() {
-        
+
     }
 
     public static int Sumar(int a, int b) {
         return a + b;
     }
     
+    public static int Restar (int a, int b){
+        return a - b;
+    }
+
 }
--- calculadora/src/main/java/test/calculadora.java
+++ calculadora/src/main/java/test/calculadora.java
@@ -1,5 +1,6 @@
 package test;
 
+import static com.mycompany.calculadora.Operaciones.Restar;
 import static com.mycompany.calculadora.Operaciones.Sumar;
 import java.util.Scanner;
 
@@ -7,14 +8,26 @@ public class calculadora {
 
     public static void main(String[] args) {
 
-        Scanner con = new Scanner(System.in);
-        System.out.println("Ingrese los valores a operar: ");
-        int valor1 = con.nextInt();
-        int valor2 = con.nextInt();
+        int resultado = 0;
+        int valor1, valor2 = 0;
+        String operacion = "";
 
-        int resultado = Sumar(valor1, valor2);
-        System.out.println("resultado = " + resultado);
-        
+        Scanner con = new Scanner(System.in);
+
+        System.out.println("Ingrese el indicador de operacion a realizar: ");
+        System.out.println("sumar" + "\n" + "restar");
+        operacion = con.nextLine();
+        System.out.println("Ingrese los valores a operar: ");
+        valor1 = con.nextInt();
+        valor2 = con.nextInt();
+
+        if (operacion.equals("sumar")) {
+            resultado = Sumar(valor1, valor2);
+            System.out.println("resultado = " + resultado);
+        } else {
+            resultado = Restar(valor1, valor2);
+            System.out.println("resultado = " + resultado);
+        }
     }
 
 }
